8b5420ebe893d26c5fdb2b46bd783b14e877ad3a
Atelier Ferrofell <kai@ferrofell.com>
2025-09-30 11:35:33 -0700

    Skill node active variable

--- SkillNode.cs
+++ SkillNode.cs
@@ -5,5 +5,9 @@ using System;
 public partial class SkillNode : Node
 {
 	[Export]
+	// Limit setting to true only if all requirements are met. Use a custom setter and/or a method
+	public bool Active { get; set; }
+
+	[Export]
 	public Godot.Collections.Array<SkillNode> Requirements { get; set; }
 }
